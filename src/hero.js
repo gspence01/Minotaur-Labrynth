28f47f4a5a3f32e21bf9f2dd4afda6191ed2f08a
Gabrielle <gabrielle01.spence@gmail.com>
2022-07-17 13:40:11 -0700

    Added hero asset picture and gif

---
+++ src/hero.js
@@ -0,0 +1,14 @@
+export default class Hero{
+    constructor(x, y, size, velocity, tileMap){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.velocity = velocity;
+        this.tileMap = tileMap;
+    }
+
+    draw(ctx){
+        
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import TileMap from "./TileScreen.js";
 
 const tileSize = 32;
+const velocity = 1;
+
 const canvas = document.getElementById("gameCanvas");
 const contxt = canvas.getContext('2d');
 
-let tileMap = new TileMap(tileSize)
+let tileMap = new TileMap(tileSize);
+const hero = tileMap.getHero(velocity);
 
 function gameLoop(){
     tileMap.drawMap(contxt);
+    Hero.draw(contxt);
 }
 
 tileMap.setCanvasSize(canvas);
 
-setInterval(gameLoop, 1000)
+setInterval(gameLoop, 1000);
